e23c571b2e05817589bd73311446594fb88577b9
Mwan3kii <agathawanja303@gmail.com>
2024-08-18 21:33:26 +0300

    Fix pycodestyle errors

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -37,8 +37,8 @@ class Server:
         """
         Retrieves page of the dataset
         """
-        assert isinstance(page, int) and page > 0, "Page must be a positive integer."
-        assert isinstance(page_size, int) and page_size > 0, "Page size must be a positive integer."
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
         start, end = index_range(page, page_size)
         dataset = self.dataset()
