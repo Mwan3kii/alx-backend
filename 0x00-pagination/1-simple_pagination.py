eafc6bcbcd6fd51921fabf5ef7343c9365524623
Mwan3kii <agathawanja303@gmail.com>
2024-08-18 20:54:00 +0300

    Check indentation

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -18,8 +18,10 @@ class Server:
     """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
+
     def __init__(self):
         self.__dataset = None
+
 
     def dataset(self) -> List[List]:
         """Cached dataset"""
@@ -30,6 +32,7 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
+
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
