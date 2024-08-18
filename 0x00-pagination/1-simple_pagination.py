55f3d73926c76e6ea281babf2b70d947ef9382d1
Mwan3kii <agathawanja303@gmail.com>
2024-08-18 21:10:43 +0300

    Check indentation

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -6,21 +6,21 @@ from typing import List, Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    Returns a tuple containing the start index and end index corresponding to the range of indexes
+    Returns a tuple containing the start index and end index
+    corresponding to the range of indexes
     to return in a list for the given pagination parameters.
     """
     start = (page - 1) * page_size
     end = page * page_size
     return start, end
 
+
 class Server:
     """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
-
     def __init__(self):
         self.__dataset = None
-
 
     def dataset(self) -> List[List]:
         """Cached dataset"""
@@ -31,7 +31,6 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
-
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
