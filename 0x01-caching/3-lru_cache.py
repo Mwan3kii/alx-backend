00d414f6b2e66cf6bbc4089ba7ce237d9c7c46fe
Mwan3kii <agathawanja303@gmail.com>
2024-08-21 23:31:30 +0300

    Try new approach

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -5,28 +5,33 @@ from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a LRU
+    algorithm when the limit is reached.
+    """
     def __init__(self):
+        """Initializes the cache.
+        """
         super().__init__()
-        self.cache_data = OrderedDict()  # Use OrderedDict to maintain order of access
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
+        """Adds an item in the cache.
+        """
         if key is None or item is None:
             return
-
-        # Add or update the item in the cache
-        if key in self.cache_data:
-            self.cache_data.move_to_end(key)  # Move the key to the end to mark it as recently used
-        self.cache_data[key] = item
-
-        # Check if we need to discard the least recently used item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            oldest_key, _ = self.cache_data.popitem(last=False)  # Remove the oldest item
-            print(f"DISCARD: {oldest_key}")
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lru_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", lru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
-        if key is None or key not in self.cache_data:
-            return None
-
-        # Move the accessed key to the end to mark it as recently used
-        self.cache_data.move_to_end(key)
-        return self.cache_data[key]
+        """Retrieves an item by key.
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
