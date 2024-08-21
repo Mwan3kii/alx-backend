f29ad1fdbe5a0bb97a227908b0f51dbeb13d4c46
Mwan3kii <agathawanja303@gmail.com>
2024-08-21 23:40:39 +0300

    Check output

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -5,41 +5,23 @@ from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """_summary_
-    """
-
     def __init__(self):
-        """_summary_
-        """
         super().__init__()
-        self.usedKeys = []
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """_summary_
+        if key is None or item is None:
+            return
 
-        Args:
-                        key (_type_): _description_
-                        item (_type_): _description_
-        """
-        if key is not None and item is not None:
-            self.cache_data[key] = item
-            if key not in self.usedKeys:
-                self.usedKeys.append(key)
-            else:
-                self.usedKeys.append(
-                    self.usedKeys.pop(self.usedKeys.index(key)))
-            if len(self.usedKeys) > BaseCaching.MAX_ITEMS:
-                discard = self.usedKeys.pop(0)
-                del self.cache_data[discard]
-                print('DISCARD: {:s}'.format(discard))
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            oldest_key, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {oldest_key}")
 
     def get(self, key):
-        """return the value in self.cache_data linked to key
-
-        Args:
-                        key (_type_): _description_
-        """
-        if key is not None and key in self.cache_data.keys():
-            self.usedKeys.append(self.usedKeys.pop(self.usedKeys.index(key)))
-            return self.cache_data.get(key)
-        return None
+        if key is None or key not in self.cache_data:
+            return None
+        self.cache_data.move_to_end(key)
+        return self.cache_data[key]
