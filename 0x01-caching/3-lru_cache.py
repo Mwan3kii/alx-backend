48d293d0d3609d31c380168bc105bdf894f013aa
Mwan3kii <agathawanja303@gmail.com>
2024-08-22 22:09:59 +0300

    Document the files

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
-"""LRU cache"""
+"""LRU cache implementation"""
+
 from collections import OrderedDict
 from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
     """Inherits from BaseCaching a caching system using MRU algorithm"""
-    
+
     def __init__(self):
         """Initialize the class."""
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """Add item in the cache"""
+        """Add item in the cache
+        Args:
+            key: The key associated with the item to cache.
+            item: The item to cache.
+
+        """
         if key is None or item is None:
             return
 
@@ -25,7 +31,13 @@ class LRUCache(BaseCaching):
             print(f"DISCARD: {oldest_key}")
 
     def get(self, key):
-        """Get item by key from the cache"""
+        """Get item by key from the cache
+
+        Args:
+            key: The key associated with the item to retrieve.
+        Returns:
+            The cached item or None if the key does not exist.
+        """
         if key is None or key not in self.cache_data:
             return None
         self.cache_data.move_to_end(key)
