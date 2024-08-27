2eef607de734323aa6142385946023c5bcccfad4
Mwan3kii <agathawanja303@gmail.com>
2024-08-27 21:08:33 +0300

    Update get locale url parameter

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -19,12 +19,15 @@ babel = Babel(app)
 @app.route('/')
 def index():
     """Funtion that renders the html template"""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 @babel.localeselector
 def get_locale():
     """Get locale from request with the decorator"""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
